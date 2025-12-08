51bf9b0c4e450426fe054731cee00bec2eda24ce
Guru1316 <guruprasadgdr1@gmail.com>
2025-12-08 13:51:14 +0530

    DOMtest.js

---
+++ JAVASCRIPT/domtest.js
@@ -0,0 +1,28 @@
+// 1) Tag Selector
+let tg = document.getElementsByTagName("h1");
+// console.log(tg);
+// console.log(tg[0]);
+console.log(tg[1]);
+
+
+// 2) Class Selector
+let cs = document.getElementsByClassName("para");
+// console.log(cs);
+// console.log(cs[0]);
+console.log(cs[1]);
+
+
+// 3) ID Selector
+let is = document.getElementById("b");
+console.log(is);
+
+
+// 4) Query Selector
+let qs = document.querySelector(".para");
+console.log(qs);
+
+// 5) Query Selector All
+let qsa = document.querySelectorAll("#b");
+// console.log(qsa);
+// console.log(qsa[0]);
+console.log(qsa[1]);

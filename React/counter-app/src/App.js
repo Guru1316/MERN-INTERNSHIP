6db61894eb7b32b8dc8f3257a19f5775f5cdef9a
Guru1316 <guruprasadgdr1@gmail.com>
2025-12-09 15:02:08 +0530

    Guessing Number Game

--- React/counter-app/src/App.js
+++ React/counter-app/src/App.js
@@ -1,59 +1,80 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-import { useState } from 'react';
-import Counter from './Counter';
-// import Component4, { Component2, Component3 } from './Component2';
-// import Component1 from './Component1';
-// import Compo1 from './Compo1';
-// import Compo2 from './Compo2';
-// import Compo3 from './Compo3';
-// import Compo4 from './Compo4';
-// import Compo5 from './Compo5';
-// import { useEffect } from 'react';
-// import Component5 from './Component5';
-// import GrandFather from './GrandFather';
+import Guess from './Number_Guessing_Website/Guess.js';
 
 function App() {
-  // let val = 0;
-  let [val, setVal] = useState(0);
-  const handleIncrement = () => {
-    // console.log("Increment button clicked");
-    // val = val + 1;
-    setVal(val + 1);
-  }
-  const handleDecrement = () => {
-    // console.log("Decrement button clicked");
-    // val = val - 1;
-    if(val>0)
+  let [ran, setRam] = useState(Math.trunc(Math.random() * 20) + 1);;
+
+  let [bg, setBg] = useState("#222")
+  let [enable, setEnable] = useState(false);
+  let [qm, setQm] = useState("?");
+  let [gv, setGv] = useState("");
+  let [msg, setMsg] = useState("Start guessing...")
+  let [scr, setScr] = useState(20);
+  let [hscr, setHscr] = useState(0);
+
+  console.log(ran);
+
+  useEffect(() =>
     {
-      setVal(val - 1);
+      document.body.style.backgroundColor = bg;
+    } , [bg]
+  )
+
+  const calRand = (e) => 
+  {
+    setRam(Number(Math.trunc(Math.random() * 20) + 1));
+  }
+
+  const handleReset = () =>
+  {
+    calRand();
+    setEnable(false);
+    setScr(20);
+    setBg("#222")
+    setMsg("Start guessing...");
+    setQm("?")
+    setGv("");
+  }
+
+  const handleCheck = () =>
+  {
+    if(gv === "")
+    {
+      alert("Please Enter A Number...");
+    }
+    else if(gv <= 0 || gv > 20)
+    {
+        setMsg("Invalid Value");
+    }
+    else if(gv === ran)
+    {
+        if(scr > hscr)
+        {
+            setHscr(scr);
+        }
+        setBg("green");
+        setMsg("Correct Value");
+        setQm(ran);
+        setScr(scr);
+        setEnable(true);
+    }
+    else if(gv > ran)
+    {
+        setScr(scr - 1);
+        setMsg("Too High");
+    }
+    else if(gv < ran)
+    {
+        setScr(scr - 1);
+        setMsg("Too Low");
     }
   }
-  // useEffect(() => {
-  //   document.body.style.backgroundColor = 'black';
-  //   document.body.style.color = 'white';
-  //  });
-  // let myw = "Bless You Grandson By GrandFather";
-  //  let msg = "Most Important Rule";
-  //  let total = 10;
-  //  let heading = "Rules For Sigma Males";
   return (
     <div className="App">
-      {/* <h1>10 Sigma Rules</h1>
-      <Component1 s={total} r={heading}></Component1>
-      <Component2></Component2>
-      <Component3></Component3>
-      <Component4></Component4>
-      <Component5></Component5>
-      <Compo1></Compo1>
-      <Compo2></Compo2>
-      <Compo3 q={msg}></Compo3>
-      <Compo4></Compo4>
-      <Compo5></Compo5> */}
-      {/* <GrandFather myWill = {myw}></GrandFather> */}
-      <h1>Counter - App</h1>
-      <Counter val = {val} handleIncrement = {handleIncrement} handleDecrement = {handleDecrement}></Counter>
+      <Guess enable = {enable} qm = {qm} msg = {msg} handleReset = {handleReset} handleCheck = {handleCheck} gv = {gv}
+       setGv = {setGv} scr={scr} hscr={hscr}></Guess>
     </div>
-    
   );
 }
 
--- React/counter-app/src/Counter.js
+++ React/counter-app/src/Counter.js
@@ -2,9 +2,23 @@ import './App.css'
 import ButtonContainer from './ButtonContainer';
 
 const Counter = ({val, handleIncrement, handleDecrement}) => {
+    let v = null;
+    if(val < 0)
+    {
+        v = <p id='Ne'>Negative Value</p>;
+    }
+    else if(val > 0)
+    {
+        v = <p id='Po'>Positive Value</p>;
+    }
+    else if(val === 0)
+    {
+        v = <p>Zero</p>;
+    }
     return(
       <div className='counter'>
         <h1>{val}</h1>
+        <p>{v}</p>
         <ButtonContainer handleIncrement = {handleIncrement} handleDecrement = {handleDecrement}></ButtonContainer>
       </div>
     )
